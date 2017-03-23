1047250dd4099555dc89584d0d4201b5b4780556
haamlezz <vongthavone@gmail.com>
2017-03-23 13:08:32 +0700

    add new
    change a bit code in form

--- add_customer.php
+++ add_customer.php
@@ -1,3 +1,4 @@
+<?php include 'config.php'; ?>
 <!DOCTYPE html>
 <html lang="en">
   <head>
@@ -31,16 +32,21 @@
         <?php
           $customer_name = mysqli_real_escape_string($con, $_POST['customer_name']);
           echo '<h3>ຊື່ລູກຄ້າ '.$customer_name.'</h3>';
-          mysqli_query("INSERT INTO bill VALUES('',CURRENT_TIMESTAMP, '$customer_name'");
+          mysqli_query($con,"INSERT INTO bill VALUES('',CURRENT_TIMESTAMP, '$customer_name'");
+          echo mysqli_error($con);
+          echo "INSERT INTO bill VALUES('',CURRENT_TIMESTAMP, '$customer_name'";
+          $rs = mysqli_query($con,"SELECT MAX(bill_id) FROM bill WHERE customer_name = '$customer_name'");
+          $row = mysqli_fetch_array($rs);
         ?>
         <form action="add_customer.php" method="post">
-
+          <input type="hidden" name="bill_id" value="<?php echo $row['bill_id']; ?>">
+          <input type="hidden" name="customer_name" value="<?php echo $row['customer_name']; ?>">
           <p>
             <label>ຊື່ລູກຄ້າ</label>
             <input type="text" name="customer_name" class="form-input" />
           </p>
           <p>
-            <input type="submit" value="ເປີດບິນໃໝ່" />
+            <input type="submit" value="ເພີ່ມລາຍການສິນຄ້າ" />
           </p>
         </form>
 
